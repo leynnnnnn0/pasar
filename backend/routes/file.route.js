070a10bbfc406654578d31227665a17089e7b377
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2024-08-25 12:49:56 +0800

    created different folder for backend

--- backend/routes/file.route.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import {extractPdfText} from "../controllers/file.controller.js";
-const router = express.Router();
-
-router.post('/extract-text', extractPdfText);
-
-export default router;
--- backend/routes/openai.route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import {dummyExam, generateExam} from "../controllers/openai.controller.js";
-const router = express.Router();
-
-router.post('/generate-questions', generateExam);
-router.post('/test', dummyExam)
-
-export default router;
-
